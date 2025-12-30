9f3985d15fa0f2034026ae10a0e4fa7f5bb09098
Thiago Soares <thiaagomarvel@gmail.com>
2025-12-30 01:37:52 -0300

    added selectors list for login spec

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,26 +1,33 @@
 // cypress/e2e/login.cy.js
 
 describe('Fluxo de Login', () => {
+  const selectorsList = {
+    usernameField: 'input[name="username"]',
+    passwordFiel: 'input[placeholder="Password"]',
+    loginButton: 'button[type="submit"]',
+    locationDashboard: '.oxd-text--h6'
+  }
+
   it('Deve fazer login com sucesso', () => {
     // Abre a página de login
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
     // Preenche os campos de login
-    cy.get('input[name="username"]').type('Admin')
-    cy.get('input[placeholder="Password"]').type('admin123')
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordFiel).type('admin123')
 
     // Clica no botão de login
-    cy.get('button[type="submit"]').click()
+    cy.get(selectorsList.loginButton).click()
 
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-text--h6').contains('Dashboard')
+    cy.get(selectorsList.locationDashboard).contains('Dashboard')
   })
 
   it('login-fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('input[name="username"]').type('test')
-    cy.get('input[placeholder="Password"]').type('test')
-    cy.get('button[type="submit"]').click()
+    cy.get(selectorsList.usernameField).type('test')
+    cy.get(selectorsList.passwordFiel).type('test')
+    cy.get(selectorsList.loginButton).click()
 
     cy.get('.oxd-alert-content--error', { timeout: 10000 })
       .should('contain.text', 'Invalid credentials')
